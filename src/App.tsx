f0407a6958c5c3fbe712333c2fb1738fc981a0c0
Tair_Huri <tair58101@gmail.com>
2025-09-21 22:12:07 +0300

    עדכון קבצים אחרונים מה-frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Campaigns from './pages/Campaigns'
-import About from './pages/About'
-
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Campaigns from "./pages/Campaigns";
+import About from "./pages/About";
+import Registration from "./pages/Registration";
+import LoginNgo from "./pages/LoginNgo";
+import DonorsNgo from "./pages/DonorsNgo";
+import RegistrationNgo from "./pages/RegistrationNgo";
+import NgoDashboard from "./pages/NgoDashboard";
+import { AuthProvider } from "./contexts/AuthContext";
+import { CampaignsProvider } from "./contexts/CampaignsContext";
+import Discover from "./pages/Discover";
+import Donate from "./pages/Donate";
+import CampaignDetails from "./pages/CampaignDetails";
+import NewCampaign from "./pages/NewCampaign";
+const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900">
-      <Navbar />
-      <div className="p-6 max-w-6xl mx-auto">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/campaigns" element={<Campaigns />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </div>
-  )
-}
+    <AuthProvider>
+      <CampaignsProvider>
+        <div className="min-h-screen bg-gray-100 text-gray-900 flex flex-col">
+          {/* סרגל ניווט עליון */}
+          <Navbar />
 
-export default App
+          {/* תוכן ראשי */}
+          <div className="flex-1 p-6 max-w-6xl mx-auto">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/discover" element={<Discover />} />
+              <Route path="/donate" element={<Donate />} />
+              <Route path="/campaigns" element={<Campaigns />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/registration" element={<Registration />} />
+              <Route path="/login/ngo" element={<LoginNgo />} />
+              <Route path="/registration/ngo" element={<RegistrationNgo />} />
+              <Route path="/donors/ngo" element={<DonorsNgo />} />
+              <Route path="/ngo/home" element={<NgoDashboard />} />
+              <Route path="/campaign/:id" element={<CampaignDetails />} />
+              <Route path="/campaigns/new" element={<NewCampaign />} />
+
+            </Routes>
+          </div>
+        </div>
+      </CampaignsProvider>
+    </AuthProvider>
+  );
+};
+
+export default App;
